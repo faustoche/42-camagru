8df471ba310556e03d816e5b89b30a823b4673e1
faustoche <faustine.crocq@gmail.com>
2026-03-04 11:44:44 +0100

    feat(front) add layout page

--- srcs/models/User.php
+++ srcs/models/User.php
@@ -36,4 +36,26 @@ class Users {
 			return false;
 		}
 	}
+
+	public function saveUser(string $username, string $email, string $password) {
+		
+		$passwordHashed = password_hash($password, PASSWORD_ARGON2ID);
+		## Insertion du user dans la table
+		$request = 'INSERT INTO users VALUES (username = :username, email = :email, password = :password)';
+
+		$statement = $this->pdoConnection->prepare($request);
+		$statement->execute([':username' => $username, ':email' => $email, ':password' => $passwordHashed]);
+
+		## On fetch sur notre variable statement
+		## Fetch va aller chercher la premiére ligne de résultat trouvée par la requête
+		## Si fetch trouve qqchose -> l'user ou l'email existe déjà alors return true
+		## Sinon return false
+		$result = $statement->fetch();
+		if ($result) {
+			echo "Cannot save user in database";
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
---
+++ srcs/views/layout.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang=en>
+	<head>
+
+		<meta charset="UTF-8"/>
+		<meta name="viewport" content="width=device-width,initial-scale=1" />
+		<title>📸 Camagru 📸</title>
+		<link rel="stylesheet" href="../public/css/styles.css">
+	</head>
+
+	<body>
+		
+		<!-- Header on the page //  -->
+		<!-- //TODO: changer les boutons de register/login -->
+		<div>
+			<header>★ CAMAGRU ★</header>
+			<button>Register</button>
+			<button>Login</button>
+		</div>
+		<div>
+			<marquee scrollamount="10"> Create, post, like and comment</marquee>
+		</div>
+
+		<div>
+			<p>Explore the most recent artworks</p>
+			<div>
+				<!-- //TODO: Ajouter une grid avec les uploads  -->
+			</div>
+			<p>xxx elements per page - 1/10</p>
+		</div>
+
+	</body>
+
+	<footer>
+
+	</footer>
+</html>
